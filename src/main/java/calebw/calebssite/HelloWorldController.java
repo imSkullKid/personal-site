9bbfe468d51271beabde4d37c5a487e8e1d137ad
Caleb White <calebadwhite@gmail.com>
2024-07-30 21:04:38 -0400

    added react

---
+++ src/main/java/calebw/calebssite/HelloWorldController.java
@@ -0,0 +1,14 @@
+package calebw.calebssite;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+        @GetMapping(path = "/hello")
+        public String helloWorld() {
+            return "Hello World!";
+        }
+
+}
