74681bacb16609a16c3ea51be355829ac598ea0f
Caleb White <calebadwhite@gmail.com>
2025-06-01 17:29:12 -0400

    stuff

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 A Website to showcase myself
 
 Run: Mvn clean install
+
+Using the terminal, navigate to the root directory: and run: mvn spring-boot:run
+
+Readme still work in progress
+
+ToDo Use This to Write Readme: https://www.freecodecamp.org/news/how-to-write-a-good-readme-file/
--- src/main/java/calebw/calebssite/HomePageController.java
+++ src/main/java/calebw/calebssite/HomePageController.java
@@ -4,9 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloWorldController {
+public class HomePageController {
 
-        @GetMapping(path = "/hello")
+        @GetMapping(path = "/api/home")
         public String helloWorld() {
             return "Hello World!";
         }
